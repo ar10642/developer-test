8cb0b38f5322286ba217a4053f8cbc7423d1ccd3
Andrew Richardson <andrew10642@gmail.com>
2020-03-23 17:52:25 +0000

    Dump out basic CSV file based on input

--- app/Http/Controllers/CsvExport.php
+++ app/Http/Controllers/CsvExport.php
@@ -2,13 +2,32 @@
 declare(strict_types=1);
 
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
 
 class CsvExport extends Controller {
-    /**
-     * Converts the user input into a CSV file and streams the file back to the user
-     */
-    public function convert()
-    {
-        return ['success' => false];
-    }
+	/**
+	 * Converts the user input into a CSV file and streams the file back to the user
+	 */
+	public function convert(Request $request)
+	{
+
+		$headers = [
+			"Content-type" => "text/csv",
+			"Content-Disposition" => "attachment; filename=CsvExport.csv",
+			"Pragma" => "no-cache",
+			"Cache-Control" => "must-revalidate, post-check=0, pre-check=0",
+			"Expires" => "0"
+		];
+
+		$export = function() use($request) {
+			$handle = fopen('php://output', 'w');
+			foreach($request->input() as $row) {
+				fputcsv($handle, $row);
+			}
+			fclose($handle);
+		};
+
+		return response()->stream($export, 200, $headers);
+
+	}
 }
